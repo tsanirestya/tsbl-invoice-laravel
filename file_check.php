e753cf12fa3502271af5ff07e640328a83daf721
Tsani Restya <tsanirestya@gmail.com>
2026-05-15 13:49:52 +0700

    Debug: Update file_check.php for environment verification

--- file_check.php
+++ file_check.php
@@ -1,9 +1,17 @@
 <?php
+date_default_timezone_set('UTC');
+echo "<h3>Server Environment</h3>";
+echo "Current Time (UTC): " . date('Y-m-d H:i:s') . "<br>";
+echo "Current Date (Ymd): " . date('Ymd') . "<br>";
+echo "Expected Token: tsbl_deploy_" . date('Ymd') . "<br>";
+echo "PHP Version: " . phpversion() . "<br>";
+echo "ZipArchive: " . (class_exists('ZipArchive') ? "Enabled" : "DISABLED") . "<br>";
+
 echo "<h3>Current Directory: " . __DIR__ . "</h3>";
 echo "<h3>Files in this directory:</h3><pre>";
 $files = scandir(__DIR__);
 foreach ($files as $file) {
-    $size = is_file($file) ? filesize($file) . " bytes" : "[DIR]";
+    $size = is_file(__DIR__ . '/' . $file) ? filesize(__DIR__ . '/' . $file) . " bytes" : "[DIR]";
     echo str_pad($file, 30) . " $size\n";
 }
 echo "</pre>";
@@ -13,10 +21,12 @@ echo "<h3>Files in ../tsbl-invoice-laravel:</h3><pre>";
 if (file_exists($appDir)) {
     $files = scandir($appDir);
     foreach ($files as $file) {
-        $size = is_file($appDir . '/' . $file) ? filesize($appDir . '/' . $file) . " bytes" : "[DIR]";
+        $filePath = $appDir . '/' . $file;
+        $size = is_file($filePath) ? filesize($filePath) . " bytes" : "[DIR]";
         echo str_pad($file, 30) . " $size\n";
     }
 } else {
     echo "Directory not found: $appDir";
 }
 echo "</pre>";
+
